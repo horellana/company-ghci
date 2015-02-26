a37e83c0100882433bdd22e5bd3cb0e30dd01b7b
Juiko <pulga.p.29@gmail.com>
2015-02-26 05:34:18 -0300

    First commit

---
+++ company-ghci.el
@@ -0,0 +1,16 @@
+(require 'company)
+(require 'haskell-process)
+
+(defun company-ghci-backend (command &optional arg &rest ignored)
+	(interactive (list 'interactive))
+	(let* ((to-complete (company-grab-symbol))
+				 (can-complete (haskell-session-maybe))
+				 (completions (when can-complete
+												(haskell-process-get-repl-completions (haskell-process)
+																															to-complete))))
+		(cl-case command
+			(interactive (company-begin-backend 'company-ghci-backend))
+			(prefix  (and can-complete to-complete))
+			(candidates completions))))
+
+(provide 'company-ghci)
