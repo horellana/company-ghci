82b3c0242d4e9ccc320d7e81cc661d4b1765ba2b
Juiko <pulga.p.29@gmail.com>
2015-02-27 05:07:03 -0300

    Added package metadata.

--- company-ghci.el
+++ company-ghci.el
@@ -1,13 +1,44 @@
+;;; company-ghci.el --- company backend which uses the current ghci process.
+
+;; Author: Hector Orellana <hofm92@gmail.com>
+;; Package-Requires: ((company "0.8.11") (haskell-mode "13"))
+;; Package-Version: 0.01
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Usage:
+
+;; (require 'company-ghci)
+;; (add-to-list 'company-backends 'company-ghci)
+;; (add-hook 'haskell-mode-hook 'company-mode)
+
+;;; Code:
+
 (require 'company)
 (require 'haskell-process)
 
 (defun company-ghci (command &optional arg &rest ignored)
-	(interactive (list 'interactive))
-	(pcase command
-		(interactive (company-begin-backend 'company-ghci))
-		(prefix  (and (haskell-session-maybe)
-									(company-grab-symbol)))
-		(candidates (cdr (haskell-process-get-repl-completions (haskell-process)
-																													 arg)))))
+  "Company backend that provides completions using the current ghci process."
+  (interactive (list 'interactive))
+  (pcase command
+    (interactive (company-begin-backend 'company-ghci))
+    (prefix  (and (haskell-session-maybe)
+		  (company-grab-symbol)))
+    (candidates (cdr (haskell-process-get-repl-completions (haskell-process)
+							   arg)))))
 
 (provide 'company-ghci)
+;;; company-ghci.el ends here
