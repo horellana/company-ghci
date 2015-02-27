a71766276c933d6bb44d364ba00d1927553e97c4
Juiko <pulga.p.29@gmail.com>
2015-02-26 22:01:45 -0300

    Removed the let* statement

--- company-ghci.el
+++ company-ghci.el
@@ -3,14 +3,11 @@
 
 (defun company-ghci (command &optional arg &rest ignored)
 	(interactive (list 'interactive))
-	(let* ((to-complete (company-grab-symbol))
-				 (can-complete (haskell-session-maybe))
-				 (completions (when can-complete
-												(haskell-process-get-repl-completions (haskell-process)
-																															to-complete))))
-		(cl-case command
-			(interactive (company-begin-backend 'company-ghci-backend))
-			(prefix  (and can-complete to-complete))
-			(candidates completions))))
+	(pcase command
+		(interactive (company-begin-backend 'company-ghci))
+		(prefix  (and (haskell-session-maybe)
+									(company-grab-symbol)))
+		(candidates (cdr (haskell-process-get-repl-completions (haskell-process)
+																													 arg)))))
 
 (provide 'company-ghci)
