23381c583f3b8e18c6d9c03fa21ec2fc88a0184c
Juiko <pulga.p.29@gmail.com>
2015-03-02 00:42:39 -0300

    Less is more ...

--- company-ghci.el
+++ company-ghci.el
@@ -31,17 +31,10 @@
 (require 'company)
 (require 'haskell)
 (require 'haskell-process)
-(require 'haskell-interactive-mode)
 
 (defun company-ghci/chomp (str)
 	"Remove trailing newline in STR."
 	(replace-regexp-in-string "\n$" "" str))
-
-(defun company-ghci/grab-line ()
-	(company-ghci/chomp
-	 (replace-regexp-in-string (haskell-interactive-prompt-regex)
-														 ""
-														 (thing-at-point 'line t))))
 
 (defun company-ghci/repl-command (cmd)
 	(when (haskell-session-maybe)
@@ -57,31 +50,21 @@
 	(company-ghci/repl-command (concat ":t " function)))
 
 (defun company-ghci/is-valid-completion (completion to-complete)
-	(and (string-match to-complete completion)
-			 (>= (length completion)
-					 (length to-complete))))
+	(string-match to-complete completion))
 
-(defun company-ghci/get-completions (str)
+(defun company-ghci/get-completions (to-complete)
 	(when (haskell-session-maybe)
-		(let* ((in-import-stm (string-match "import" str))
-					 (to-complete (if in-import-stm
-														(second (split-string str " ")) str))
-					 (res (cl-remove-if
-								 (lambda (a) (not (company-ghci/is-valid-completion a
-																																		to-complete)))
-								 (haskell-process-get-repl-completions (haskell-process)
-																											 str))))
-			(if (string-match "import" str)
-					(mapcar (lambda (a) (concat "import " a)) res)
-				res))))
+		(remove-if-not (lambda (a) (company-ghci/is-valid-completion a to-complete))
+									 (haskell-process-get-repl-completions (haskell-process)
+																												 to-complete))))
 
 ;;;###autoload
 (defun company-ghci (command &optional arg &rest ignored)
-  "Company backend that provides completions using the current ghci process."
-  (interactive (list 'interactive))
-  (cl-case command
-    (interactive (company-begin-backend 'company-ghci))
-    (prefix  (and (haskell-session-maybe) (company-ghci/grab-line)))
+	"Company backend that provides completions using the current ghci process."
+	(interactive (list 'interactive))
+	(cl-case command
+		(interactive (company-begin-backend 'company-ghci))
+		(prefix  (and (haskell-session-maybe) (company-grab-symbol)))
 		(candidates (company-ghci/get-completions arg))
 		(meta (company-ghci/get-signature arg))))
 
