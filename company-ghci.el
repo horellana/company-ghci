0a8b397fc78228ba2c1ec71e5c760f56387ebf5f
Hector Orellana <pulga.p.29@gmail.com>
2015-09-06 00:51:00 -0300

    fixed indentation.

--- company-ghci.el
+++ company-ghci.el
@@ -33,40 +33,40 @@
 (require 'haskell-process)
 
 (defun company-ghci/chomp (str)
-	"Remove trailing newline in STR."
-	(replace-regexp-in-string "\n$" "" str))
+  "Remove trailing newline in STR."
+  (replace-regexp-in-string "\n$" "" str))
 
 (defun company-ghci/repl-command (cmd)
-	(when (haskell-session-maybe)
-		(let ((response (company-ghci/chomp (haskell-process-queue-sync-request
-																				 (haskell-process)
-																				 cmd))))
-			(unless (and (stringp response)
-									 (string-match "interactive" response))
-				response))))
+  (when (haskell-session-maybe)
+    (let ((response (company-ghci/chomp (haskell-process-queue-sync-request
+					 (haskell-process)
+					 cmd))))
+      (unless (and (stringp response)
+		   (string-match "interactive" response))
+	response))))
 
 (defun company-ghci/get-signature (function)
-	"Uses the :t repl command to get the signature of FUNCTION."
-	(company-ghci/repl-command (concat ":t " function)))
+  "Uses the :t repl command to get the signature of FUNCTION."
+  (company-ghci/repl-command (concat ":t " function)))
 
 (defun company-ghci/is-valid-completion (completion to-complete)
-	(string-match to-complete completion))
+  (string-match to-complete completion))
 
 (defun company-ghci/get-completions (to-complete)
-	(when (haskell-session-maybe)
-		(remove-if-not (lambda (a) (company-ghci/is-valid-completion a to-complete))
-									 (haskell-process-get-repl-completions (haskell-process)
-																												 to-complete))))
+  (when (haskell-session-maybe)
+    (cl-remove-if-not (lambda (a) (company-ghci/is-valid-completion a to-complete))
+		      (haskell-process-get-repl-completions (haskell-process)
+							    to-complete))))
 
 ;;;###autoload
 (defun company-ghci (command &optional arg &rest ignored)
-	"Company backend that provides completions using the current ghci process."
-	(interactive (list 'interactive))
-	(cl-case command
-		(interactive (company-begin-backend 'company-ghci))
-		(prefix  (and (haskell-session-maybe) (company-grab-symbol)))
-		(candidates (company-ghci/get-completions arg))
-		(meta (company-ghci/get-signature arg))))
+  "Company backend that provides completions using the current ghci process."
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-ghci))
+    (prefix  (and (haskell-session-maybe) (company-grab-symbol)))
+    (candidates (company-ghci/get-completions arg))
+    (meta (company-ghci/get-signature arg))))
 
 (provide 'company-ghci)
 ;;; company-ghci.el ends here
